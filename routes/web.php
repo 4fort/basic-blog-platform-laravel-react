c48fdf2c0042a21785ed2d1f77e1b3d5b172d866
miskocia <dave64342@gmail.com>
2025-06-17 22:11:57 +0800

    Fix: Enable dashboard route to resolve undefined route error

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,11 @@ Route::get('/api/tags', function () {
     return response()->json(Tag::all());
 });
 
-/* Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
-}); */
+}); 
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
